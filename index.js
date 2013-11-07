938040dbf9e3ab1ce2aa5fa7f669527e0a71364a
beezzoo <beezzoo@gmail.com>
2013-11-07 16:16:03 +0400

    Переименовал файл сервера

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
 console.log("Hello World");
-
-var http = require("http");
-
-http.createServer(function(request, response) {
- response.writeHead(200, {"Content-Type": "text/plain"});
- response.write("Hello World");
- response.end();
-}).listen(3000);
